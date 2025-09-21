48c8d86b6f6edd4f211bfea0ccdfe0f63ca57d5f
accentiquetunisie-code <accentiquetunisie@gmail.com>
2025-09-21 13:24:43 +0200

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1433,7 +1433,7 @@ export const products: Product[] = [
     price: 18,
     category: ['earings'],
     images: [
-      '/product_data/earings/boucle7.jpeg'
+      '/product_data/earings/boucle13.png'
     ],
     description: 'boucle-d\'oreilles solitaire avec pierre centrale et anneau doré finement ciselé.',
     features: [
