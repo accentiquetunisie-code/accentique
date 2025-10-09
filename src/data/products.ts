1f1d6d40b77bd0aa4a6ed81bd92f0ba6a3a36631
accentiquetunisie-code <accentiquetunisie@gmail.com>
2025-10-09 20:12:03 +0200

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -207,7 +207,7 @@ export const products: Product[] = [
     originalPrice: 110,
     category: ['package'],
     images: [
-      '/product_data/watches/montre+bracelet/montreetbracelet10.jpeg'
+      '/product_data/watches/montre+bracelet/IMG_1298.jpeg'
     ],
     description: 'Une montre élégante et 1 bracelet en acier inoxydable.',
     features: [
