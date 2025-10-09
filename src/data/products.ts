e651187b143474757d5f4eacd110261b3f93ca61
accentiquetunisie-code <accentiquetunisie@gmail.com>
2025-10-09 18:25:57 +0200

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -12,7 +12,7 @@ export interface Product {
   name: string;
   price: number;
   originalPrice?: number;
-  category: ('watches' | 'bracelets' | 'rings' | 'earings' | 'gift-sets' | 'fan' | 'necklace' | 'package' | 'other' )[];
+  category: ('watches' | 'bracelets' | 'rings' | 'earings' | 'gift-sets' | 'fan' | 'necklace' | 'package' | 'mount' | 'other' )[];
   images: string[];
   description: string;
   features: string[];
@@ -1628,5 +1628,6 @@ export const categories = [
   { id: 'earings', name: 'boucles d oreilles', count: products.filter(p => p.category.includes('earings')).length },
   { id: 'fan', name: 'Éventail', count: products.filter(p => p.category.includes('fan')).length },
   { id: 'package', name: 'pack', count: products.filter(p => p.category.includes('package')).length },
-  { id: 'other', name: 'autres', count: products.filter(p => p.category.includes('other')).length }
+  { id: 'other', name: 'autres', count: products.filter(p => p.category.includes('other')).length },
+  { id: 'mount', name: 'Montures', count: products.filter(p => p.category.includes('mount')).length }
 ];
