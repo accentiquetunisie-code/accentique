09856c7d7772eb8bd027ee33d2138b7838d61e2e
accentiquetunisie-code <accentiquetunisie@gmail.com>
2025-10-31 12:36:21 +0100

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -2600,7 +2600,7 @@ export const products: Product[] = [
       'Mouvement quartz de précision',
       'Bracelet ajustable'
     ],
-    inStock: true,
+    inStock: false,
     isNew: false,
     isFreeDelivery: true,
     materials: ['Acier inoxydable'],
