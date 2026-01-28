5b72cec8f62d8023fd72ff8e011548a82f540bf2
accentiquetunisie-code <accentiquetunisie@gmail.com>
2026-01-28 16:07:00 +0100

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -246,10 +246,10 @@ price: 89,
     originalPrice: 110,
     category: ['package'],
     images: [
-      '/product_data/watches/montre+bracelet/IMG_5485.jpeg',
-      '/product_data/watches/montre+bracelet/IMG_5476.jpeg',
-      '/product_data/watches/montre+bracelet/IMG_5477.jpeg',
-      '/product_data/watches/montre+bracelet/IMG_5486.jpeg',
+      '/product_data/watches/IMG109.jpeg',
+      '/product_data/watches/IMG110.jpeg',
+      '/product_data/watches/IMG111.jpeg',
+      '/product_data/watches/IMG112.jpeg'
     ],
     description: 'Montre + bracelet , finition acier inoxydable, dans un coffret LOVE garni de roses ',
     features: [
